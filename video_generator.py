started working on videoscanner class, moved generate_logit to i3d_inception.py and fixed an error in vieo_generator.py regarding normalization

video_generator.py
@@ -45,7 +45,7 @@ class VideoGenerator:
                 break
         
         output = np.array(frames)
-        output = output / 255
+        output = (output / 255) * 2 - 1
                 
         return output
 