29c281a13d04d2f9a16f193937c77de62a6b36b5
dshin1331@gmail.com <dshin1331@gmail.com>
2019-06-13 12:55:13 -0400

    added data augmentation, training code and a separate model build code

---
+++ build_rgb_model.py
@@ -0,0 +1,46 @@
+# Python code to build an RGB-only Inflated3D network
+# Outputs a file ('test_model.hdf5') in the same directory
+
+# I3D model functions
+from models.i3d_inception import Inception_Inflated3d
+from models.i3d_inception import generate_logit
+
+# Keras models and layers
+from keras.models import Model
+from keras.layers import Activation
+from keras.layers import Dropout
+
+# Single stream 3D convolution model in RGB channel using ImageNet/Kinetics weights
+def RGB_model(NUM_FRAMES, FRAME_HEIGHT, FRAME_WIDTH, NUM_CLASSES, dropout_prob):
+
+    rgb_model = Inception_Inflated3d(
+        include_top=False,
+        weights='rgb_imagenet_and_kinetics',
+        input_shape=(NUM_FRAMES, FRAME_HEIGHT, FRAME_WIDTH, 3))
+
+    x1 = rgb_model.layers[-1].output
+    x1 = Dropout(dropout_prob)(x1)
+
+    x1 = generate_logit(x1, '1x1_Conv3d_rgb_logits', NUM_CLASSES)
+
+    x = Activation('softmax', name='prediction')(x1)
+
+    model = Model(input=rgb_model.input, output=x)
+
+    return model
+
+if __name__ == '__main__':
+    
+    # Input dimensions and dropout probability
+    NUM_FRAMES=150
+    FRAME_HEIGHT=224
+    FRAME_WIDTH=224
+    NUM_CLASSES=2
+    dropout_prob=0.5
+    
+    # Instantiate model
+    test_model = RGB_model(NUM_FRAMES, FRAME_HEIGHT, FRAME_WIDTH, NUM_CLASSES, dropout_prob)    
+    
+    # Save the model
+    test_model.save('test_model.hdf5')
+   
--- utils/video_generator.py
+++ utils/video_generator.py
@@ -4,21 +4,21 @@ import os
 import cv2
 import numpy as np
 
-# Using template code from https://github.com/jphdotam/keras_generator_example
+# Based on template from https://github.com/jphdotam/keras_generator_example
 
 class VideoGenerator:
 
-    def __init__(self, train_dir, test_dir, dims, batch_size=2, shuffle=True, file_ext=".mkv"):
+    def __init__(self, train_dir, val_dir, dims, batch_size=2, shuffle=True, file_ext=".mkv"):
         self.train_dir = train_dir
-        self.test_dir = test_dir
+        self.val_dir = val_dir
         self.frames, self.width, self.height, self.channels = dims
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.file_ext = file_ext
 
         self.filenames_train = self.get_filenames(train_dir)
-        if self.test_dir:
-            self.filenames_test = self.get_filenames(test_dir)
+        if self.val_dir:
+            self.filenames_val = self.get_filenames(val_dir)
 
         self.classname_by_id = {i: cls for i, cls in
                                 enumerate({os.path.basename(os.path.dirname(file)) for file in self.filenames_train})}
@@ -26,36 +26,48 @@ class VideoGenerator:
 
         self.n_classes = len(self.classname_by_id)
         assert self.n_classes == len(
-            self.id_by_classname), "Number of unique classes for training set isn't equal to testing set"
+            self.id_by_classname), "Number of unique classes for training set isn't equal to validation set"
 
     def get_filenames(self, dir):
         filenames = glob.glob(os.path.join(dir, f"**/*{self.file_ext}"))
         return filenames
     
     # Extracts frames from clip using OpenCV
-    def vid2npy(self, filename):
+    def vid2npy(self, filename, num_frames=250, random_start=False):
+        
         cap = cv2.VideoCapture(filename)
         frames =[]
-        while True:
+        total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        
+        if random_start:
+            start_frame = random.randint(0,total_frames - num_frames)
+            cap.set(1, start_frame)
+        
+        for _ in range(num_frames):
             ret, frame = cap.read()
+            
             if ret == True:
                 frame = cv2.resize(frame, (self.width,self.height), interpolation=cv2.INTER_AREA)
                 frames.append(frame)
+                
             else:
                 break
         
         output = np.array(frames)
         output = (output / 255) * 2 - 1
+        
+        # zero-pad frames if not enough frames
+        if output.shape[0] < num_frames:
+            output = np.concatenate(np.zeros((num_frames - output.shape[0], output.shape[1], output.shape[2], output.shape[3])))
                 
         return output
 
-    def generate(self, train_or_test, rotation_range=None, heigt_shift_range=None, width_shift_range=None,
-                 shear_range=None, zoom_range=None, horizontal_flip=None, vertical_flip=None, brightness_range=None):
+    def generate(self, train_or_val, horizontal_flip=False, random_crop=False, random_start=False):
 
-        if train_or_test == 'train':
+        if train_or_val == 'train':
             dir = self.train_dir
-        elif train_or_test == 'test':
-            dir = self.test_dir
+        elif train_or_val == 'val':
+            dir = self.val_dir
         else:
             raise ValueError
 
@@ -69,19 +81,38 @@ class VideoGenerator:
             for i in range(n_batches):
                 # print(f"Slicing {i*self.batch_size}:{(i+1)*self.batch_size}")
                 filenames_batch = filenames[i * self.batch_size:(i + 1) * self.batch_size]
-                x, y = self.__generate_data_frome_batch_file_names(filenames_batch)
+                x, y = self.__generate_data_frome_batch_file_names(filenames_batch,
+                                                                   train_or_val,
+                                                                   horizontal_flip,
+                                                                   random_crop,
+                                                                   random_start)
                 yield x, y
 
-    def __generate_data_frome_batch_file_names(self, filenames_batch):
+    def __generate_data_frome_batch_file_names(self, filenames_batch, train_or_val, horizontal_flip, random_crop, random_start):
         data = []
         labels = []
 
         for i, filename in enumerate(filenames_batch):
-            npy = self.vid2npy(filename)
-            try:
-                npy = npy[npy.files[0]] # If an npz file we need to get the data out using the filename as a key
-            except:
-                pass
+            
+            # Data augmentation implements horizontal flip and random crop (temporal and spatial)
+            if train_or_val == 'train':
+                npy = self.vid2npy(filename, num_frames=150, random_start=True)
+
+                if horizontal_flip:
+                    flip = random.random()
+                    if flip > 0.5:
+                        npy = np.fliplr(npy)
+                if random_crop:
+                    horizontal_crop = random.randint(0, npy.shape[2] - npy.shape[1])
+                    npy = npy[:,:,horizontal_crop:,:]
+                    assert npy.shape[1] == npy.shape[2]
+            
+            # Center crop all validation clips
+            elif train_or_val == 'val':
+                npy = self.vid2npy(filename, num_frames=250, random_start=False)
+
+                horizontal_crop = (npy.shape[2] - npy.shape[1])//2
+                npy = npy[:,:,horizontal_crop:,:]
 
             if len(npy.shape) == 3:  # Add colour channel to B&W images
                 npy = np.expand_dims(npy, axis=-1)
